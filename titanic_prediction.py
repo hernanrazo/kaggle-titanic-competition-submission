98437597d56da4ab044f1e52011119907cf64070
Hernan Razo <hernanrazo@gmail.com>
2018-07-16 23:30:23 -0500

    Added facetGrid for pclass analysis

--- titanic_prediction.py
+++ titanic_prediction.py
@@ -19,23 +19,32 @@ print(' ')
 fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize = (10, 4))
 women = train_df[train_df['Sex'] == 'female']
 men = train_df[train_df['Sex'] == 'male']
-ax = sns.distplot(women[women['Survived'] == 1].Age.dropna(), 
+distplot = sns.distplot(women[women['Survived'] == 1].Age.dropna(), 
 	bins = 18, label = 'Survived', ax = axes[0], kde = False)
-ax = sns.distplot(women[women['Survived'] == 0].Age.dropna(), 
+distplot = sns.distplot(women[women['Survived'] == 0].Age.dropna(), 
 	bins = 40, label = ' Did Not Survive', ax = axes[0], kde = False)
-ax.legend()
-ax.set_title("Female Passengers")
-ax = sns.distplot(men[men['Survived'] == 1].Age.dropna(), 
+distplot.legend()
+distplot.set_title("Female Passengers")
+distplot = sns.distplot(men[men['Survived'] == 1].Age.dropna(), 
 	bins = 18, label = 'Survived', ax = axes[1], kde = False)
-ax = sns.distplot(men[men['Survived'] == 0].Age.dropna(), 
+distplot = sns.distplot(men[men['Survived'] == 0].Age.dropna(), 
 	bins = 40, label = 'Did Not Survive', ax = axes[1], kde = False)
-ax.legend()
-ax.set_title('Male Passengers')
-fig = ax.get_figure()
+distplot.legend()
+distplot.set_title('Male Passengers')
+fig = distplot.get_figure()
 fig.savefig(graph_folder_path + 'survival_by_gender_distplot.png')
 
 #make facetgrid for survival based on gender, port embarked, and pclass
+facetGrid = sns.FacetGrid(train_df, row = 'Embarked', size = 4.5, aspect = 1.6)
+facetGrid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette = None, 
+	order = None, hue_order = None)
+facetGrid.add_legend()
+facetGrid.savefig(graph_folder_path + 'p_s_s_facetgrid.png')
 
+#pclass seems to have a strong correlation with survival so
+#make a barplot showing chance of survival for each pclass
+sns.barplot(x = 'Pclass', y = 'Survived', data = train_df)
+sns.barplot.savefig(graph_folder_path + 'pclass_barplot.png')
 
 
 
